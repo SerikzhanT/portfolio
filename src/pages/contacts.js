6936a867db3d1b93a512ac8f6b2b573b0195829d
Serikzhan <tlekkaliserik@icloud.com>
2023-12-11 18:08:26 +0500

    update g

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -7,18 +7,18 @@ const Contacts = () => {
         <ul className="content-list">
           <li className="content-list__item">
             <h2 className="title-2">Location</h2>
-            <p>Moscow, Russia</p>
+            <p>Kazakhstan, Almaty</p>
           </li>
           <li className="content-list__item">
             <h2 className="title-2">Telegram / WhatsApp</h2>
             <p>
-              <a href="tel:+79051234567">+7 (905) 123-45-67</a>
+              <a href="tel:+79051234567">+7 (708) 572 28 98</a>
             </p>
           </li>
           <li className="content-list__item">
             <h2 className="title-2">Email</h2>
             <p>
-              <a href="mailto:webdev@protonmail.com">webdev@protonmail.com</a>
+              <a href="mailto:tlekkaliserik@icloud.com">tlekkaliserik@icloud.com</a>
             </p>
           </li>
         </ul>
